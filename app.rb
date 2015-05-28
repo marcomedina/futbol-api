3876fb07a035a75ceb67982d076316d9a6a27e4e
Marco Medina <marco@wizeline.com>
2015-05-28 13:26:02 -0500

    Comming Games Included

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ load 'mediotiempo.rb'
 
 get '/games' do
   content_type :json
+  games = Mediotiempo.get_all_games
+  games.to_json
+end
+
+get '/games/live' do
+  content_type :json
   games = Mediotiempo.get_live_games
+  games.to_json
+end
+
+get '/games/coming' do
+  content_type :json
+  games = Mediotiempo.get_coming_games
   games.to_json
 end
--- mediotiempo.rb
+++ mediotiempo.rb
@@ -1,19 +1,40 @@
 load 'scraper.rb'
 
 class Mediotiempo
-  def self.get_live_games
-    games = []
-    doc = Scraper.new('http://m.mediotiempo.com/ticker_completo.php').fetch
-    doc.css('.tickers .fila-ticker-vivo').each do |ticker|
+  URL = 'http://m.mediotiempo.com/ticker_completo.php'
 
+  def self.elements_to_games(elements, game_status)
+    games = []
+    elements.each do |ticker|
       games << {
-        local_name: ticker.css('td')[1].text.strip,
-        visitor_name: ticker.css('td')[7].text.strip,
+        local_name: ticker.css('td')[1].text.strip.encode("iso-8859-1").force_encoding("utf-8"),
+        visitor_name: ticker.css('td')[7].text.strip.encode("iso-8859-1").force_encoding("utf-8"),
         local_goals: ticker.css('td')[2].css('td.score1').text.strip,
-        visitor_goals: ticker.css('td')[2].css('td.score2').text.strip
+        visitor_goals: ticker.css('td')[2].css('td.score2').text.strip,
+        time: ticker.css('td')[2].css('tr')[1].text.strip,
+        status: game_status
       }
     end
-
     games
+  end
+
+  def self.get_all_games
+    games = []
+    doc = Scraper.new(URL).fetch
+    games << self.elements_to_games(doc.css('.tickers .fila-ticker-vivo'), 'live')
+    games << self.elements_to_games(doc.css('.tickers .fila-ticker'), 'coming')
+    games.flatten
+  end
+
+  def self.get_live_games
+    games = []
+    doc = Scraper.new(URL).fetch
+    self.elements_to_games(doc.css('.tickers .fila-ticker-vivo'), 'live')
+  end
+
+  def self.get_coming_games
+    games = []
+    doc = Scraper.new(URL).fetch
+    self.elements_to_games(doc.css('.tickers .fila-ticker'), 'coming')
   end
 end
